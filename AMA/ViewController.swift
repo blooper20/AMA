f29bab675a1ebda2723a7fb4e582619a476000d7
blooper20 <yjw127588@gmail.com>
2024-04-02 20:06:47 +0900

    Initial Commit

---
+++ AMA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMA
+//
+//  Created by DwaeWoo on 2024/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
